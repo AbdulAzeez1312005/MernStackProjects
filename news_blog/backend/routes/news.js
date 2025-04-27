df594474d12e62d0402d0ad697e4cf8135939661
AbdulAzeez1312005 <151101928+AbdulAzeez1312005@users.noreply.github.com>
2025-04-27 15:17:58 +0530

    Update news.js

--- news_blog/backend/routes/news.js
+++ news_blog/backend/routes/news.js
@@ -2,9 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
-const NEWS_API_URL =
-  "https://newsapi.org/v2/top-headlines?country=in&apiKey=52e5bca649284c7ca5de4d02c723d5c3"; // Replace with your API key
-
+const NEWS_API_URL = "https://newsapi.org/v2/top-headlines?country=in&apiKey=YOUR_NEWS_API_KEY";
 router.get("/", async (req, res) => {
   try {
     const response = await axios.get(NEWS_API_URL);
